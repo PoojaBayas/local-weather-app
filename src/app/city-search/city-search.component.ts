d062ae3492fa482d379f4fd7063638372f4c4a0a
PoojaBayas <pooja.bayas32@gmail.com>
2020-02-22 18:33:56 -0800

    completed the local weather app functionality

--- src/app/city-search/city-search.component.ts
+++ src/app/city-search/city-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 import { FormControl, Validators } from "@angular/forms";
 
 import { WeatherService } from "../weather.service";
@@ -9,6 +9,7 @@ import { debounceTime } from "rxjs/operators";
   styleUrls: ["./city-search.component.css"]
 })
 export class CitySearchComponent implements OnInit {
+  @Output() searchEvent = new EventEmitter<string>();
   search = new FormControl("", Validators.minLength(3));
   constructor(private weatherService: WeatherService) {}
 
@@ -17,13 +18,7 @@ export class CitySearchComponent implements OnInit {
       .pipe(debounceTime(1000))
       .subscribe((searchValue: string) => {
         if (!this.search.invalid && searchValue) {
-          const userInput = searchValue.split(",").map(s => s.trim());
-          this.weatherService
-            .getCurrentWeather(
-              userInput[0],
-              userInput.length > 1 ? userInput[1] : undefined
-            )
-            .subscribe(data => console.log(data));
+          this.searchEvent.emit(searchValue);
         }
       });
   }
--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,27 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { ICurrentWeather } from '../icurrent-weather';
+import { Component, OnInit, Input } from "@angular/core";
+import { ICurrentWeather } from "../icurrent-weather";
 
-import { IWeatherService } from '../iweather-service';
-import { WeatherService } from '../weather.service';
+import { IWeatherService } from "../iweather-service";
+import { WeatherService } from "../weather.service";
 
 @Component({
-  selector: 'app-current-weather',
-  templateUrl: './current-weather.component.html',
-  styleUrls: ['./current-weather.component.css']
+  selector: "app-current-weather",
+  templateUrl: "./current-weather.component.html",
+  styleUrls: ["./current-weather.component.css"]
 })
 export class CurrentWeatherComponent implements OnInit {
-  current:ICurrentWeather
-  constructor(private weatherService:WeatherService) {
-    this.current={
-      
-    }as ICurrentWeather
+  @Input() current: ICurrentWeather;
+  constructor(private weatherService: WeatherService) {}
 
-   }
-
-  ngOnInit() {
-    this.weatherService.getCurrentWeather('Bellevue','US').subscribe(
-      data =>this.current = data)
-    
-  }
-
+  ngOnInit() {}
 }
